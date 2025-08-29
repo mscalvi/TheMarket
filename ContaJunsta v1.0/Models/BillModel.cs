45dcd963e70387e14f74c32b315eb46b7a4c8375
Marcelo Scalvi <scalvi.marcelo@gmail.com>
2025-08-29 00:33:59 -0300

    ContaJunsta v1.0

---
+++ ContaJunsta v1.0/Models/BillModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ContaJunsta.Models;
+public class BillModel
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string EventId { get; set; } = "";
+    public string Description { get; set; } = "";
+    public int Cents { get; set; } // +despesa / -ganho
+    public string ResponsiblePersonId { get; set; } = "";
+    public List<string> ParticipantIds { get; set; } = new();
+    public string CreatedAt { get; set; } = DateTime.UtcNow.ToString("o");
+}
---
+++ ContaJunsta v1.0/Models/EventModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ContaJunsta.Models;
+public class EventModel
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Name { get; set; } = "";
+    public string Status { get; set; } = "Open";
+    public string CreatedAt { get; set; } = DateTime.UtcNow.ToString("o");
+    public string? ClosedAt { get; set; }
+}
