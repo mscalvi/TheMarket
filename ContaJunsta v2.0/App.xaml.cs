a3fe177db7f4b96318360a7012a83fc62fcdcdfe
Marcelo Scalvi <scalvi.marcelo@gmail.com>
2025-09-04 12:48:44 -0300

    Obsolete Version

---
+++ ContaJunsta v2.0/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ContaJunsta
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ ContaJunsta v2.0/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ContaJunsta
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
